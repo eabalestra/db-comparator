1bc3323e1428471b0d361a3b6b4da17d3bc29ab7
eabalestra <agusbalestra0@gmail.com>
2024-10-29 21:25:10 -0300

    equals for StoredProcedureColumn and StoredProcedure

--- src/main/java/storedprocedure/StoredProcedure.java
+++ src/main/java/storedprocedure/StoredProcedure.java
@@ -5,50 +5,79 @@ import java.util.List;
 
 import storedprocedure.column.StoredProcedureColumn;
 
+/**
+ * Represents a stored procedure in a database, defined by a unique name and a list of associated columns.
+ * <p>
+ * Each stored procedure has a name to identify it and a collection of columns (parameters or return types) that specify its structure.
+ * This class provides methods to retrieve the stored procedure's name, add columns, and compare stored procedures based on their attributes.
+ * </p>
+ */
 public class StoredProcedure {
     private String name;
     private List<StoredProcedureColumn> columns;
 
+    /**
+     * Constructs a new StoredProcedure with the specified name.
+     *
+     * @param name the unique name of the stored procedure
+     */
     public StoredProcedure(String name) {
         this.name = name;
         columns = new ArrayList<>();
     }
 
+    /**
+     * Returns the name of the stored procedure.
+     *
+     * @return the name of the stored procedure
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Adds a column to the stored procedure.
+     *
+     * @param column the column to be added, representing a parameter or return type
+     */
     public void addColumn(StoredProcedureColumn column) {
         columns.add(column);
     }
 
-    // @Override
-    // public boolean equals(Object f) {
+    /**
+     * Compares this stored procedure to another object for equality.
+     * <p>
+     * Stored procedures are considered equal if they have the same name and identical columns.
+     * </p>
+     *
+     * @param o the object to compare with this stored procedure
+     * @return true if the specified object is equal to this stored procedure, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
-    // if (this == f) {
-    // return true;
-    // }
-    // if (f == null || getClass() != f.getClass()) {
-    // return false;
-    // }
+        StoredProcedure that = (StoredProcedure) o;
 
-    // StoredProcedure function = (StoredProcedure) f;
+        if (!name.equals(that.name))
+            return false;
 
-    // if (!name.equals(function.name)) {
-    // return false;
-    // }
-    // if (!returnType.equals(function.returnType)) {
-    // return false;
-    // }
-    // if (!listParams.equals(function.listParams)) {
-    // return false;
-    // }
-    // return true;
-    // }
+        return columns.equals(that.columns);
+    }
 
+    /**
+     * Returns a string representation of the stored procedure.
+     * <p>
+     * The string includes the stored procedure's name and a list of columns for easier inspection and debugging.
+     * </p>
+     *
+     * @return a string representation of the stored procedure
+     */
     @Override
     public String toString() {
         return "StoredProcedure{" + "name='" + name + '\'' + ", columns=" + columns + '}';
     }
-
 }
--- src/main/java/storedprocedure/column/StoredProcedureColumn.java
+++ src/main/java/storedprocedure/column/StoredProcedureColumn.java
@@ -3,24 +3,44 @@ package storedprocedure.column;
 import table.column.ColumnType;
 
 /**
- * StoredProcedureColumn
+ * Represents a column in a stored procedure, defined by a parameter type, name, data type, and order.
  */
 public class StoredProcedureColumn {
     StoredProcedureColumnType parameterType;
     String columnName;
-    ColumnType type;
+    ColumnType columnType;
     String columnOrder;
 
     public StoredProcedureColumn(StoredProcedureColumnType parameterType, String columnName, ColumnType type, String columnOrder) {
         this.parameterType = parameterType;
         this.columnName = columnName;
-        this.type = type;
+        this.columnType = type;
         this.columnOrder = columnOrder;
     }
 
     @Override
     public String toString() {
         return "StoredProcedureColumn [columnName=" + columnName + ", columnOrder=" + columnOrder + ", parameterType="
-                + parameterType + ", type=" + type + "]";
+                + parameterType + ", type=" + columnType + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        StoredProcedureColumn that = (StoredProcedureColumn) o;
+
+        if (parameterType != that.parameterType)
+            return false;
+        if (!columnName.equals(that.columnName))
+            return false;
+        if (columnType != that.columnType)
+            return false;
+
+        return columnOrder.equals(that.columnOrder);
     }
 }
+
